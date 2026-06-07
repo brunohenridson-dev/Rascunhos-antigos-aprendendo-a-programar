4fbaf19d996b8c6fca109a27b6c2a942baaaecc5
brunohenridson-dev <brunohenridson@gmail.com>
2026-06-07 12:10:16 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,50 +1,26 @@
-using System;
-namespace qualquercoisa
+using system;
+namespace arrays
 {
-    class seila
-    {
-        public string nome {get; set; }
-        public int idade {get; set; }
-        public int verificarstatus()
-        {
-            if (idade< 18)
-            {
-                return 0;
-            }
-            return 1;
-        }
-        public void mostrarmensagem()
-        {
-            if (idade < 0)
-            {
-                Console.WriteLine("voce é menor kkkk")
-                return;
-            }
-            Console.WriteLine(&"nome: {nome}, idade: {idade}");
-        }
-    }
-    class program
-    {
-        static void main(string[]args)
-        {
-            ProducesResponseTypeMetadata p1 = new Pessoa
-            {
-                Nome + "bruno",
-                idade = 20
-            };
-            Pessoa p2 new pessoa 
-            {
-            Nome = "joao"
-            idade = 15
-            }
-            int status1 = p1.VerificarStatus();
-            int status2 = p2.VerificarStatus();
-            Console.WriteLine($"status p1: {status1}");
-            Console.WriteLine($"status p2:{status2}");
-            Console.WriteLine("-----------------"):
-            p1.MostrarMensagem();
-            p2.MostrarMensagem();
-        }
-    }
-}
+public class fazendoarrays
+{
+    static void main 
+string [] calculandosete = new string[7]
+Console.WriteLine ("escolha um numero pra somar");
+calculandosete[0]
+Console.WriteLine ("escolha um numero pra somar");
+calculandosete[1]
+Console.WriteLine ("escolha um numero pra somar");
+calculandosete[2]
+Console.WriteLine ("escolha um numero pra somar");
+calculandosete[3]
+Console.WriteLine ("escolha um numero pra somar");
+calculandosete[4]
+Console.WriteLine ("escolha um numero pra somar");
+calculandosete[5]
+Console.WriteLine ("escolha um numero pra somar");
+calculandosete[6]
+for (int i = 0 <= 6; 1++);
 
+Console.WriteLine("esse foi o resultado da sua soma"+ calculandosete);
+}
+}
